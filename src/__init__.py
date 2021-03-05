a4ce9c9da6169a71c4bd54039ea8363cc7a097e2
Sam Penny <sam1penny@gmail.com>
2021-03-05 14:50:42 +0000

    changed icon path for new layout

--- src/__init__.py
+++ src/__init__.py
@@ -21,7 +21,7 @@ def bold(s):
 
 def display(deck_browser, content):
     content.stats += """<br><link rel="stylesheet" type="text/css" href="{}/styles.css"/><h3 style="display: inline-block;margin: 0 20px" >Events</h3>
-    <div class="settings-btn" onclick='pycmd(\"add-new-event\");'><img src="{}/images/settings.png"></div>""".format(
+    <div class="settings-btn" onclick='pycmd(\"add-new-event\");'><img src="{}/icons/settings.png"></div>""".format(
         base_url, base_url)
     sortType = config['sort']
     data = cursor.execute(
